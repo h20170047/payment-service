12b216f6c14680716e60c0d1fc8b3a81b33bd23e
h20170047 <46812751+h20170047@users.noreply.github.com>
2024-04-11 14:38:42 +0530

    Adding spring cloud stream- kafka

--- src/main/java/com/svj/consumer/OrderProcessingConsumer.java
+++ src/main/java/com/svj/consumer/OrderProcessingConsumer.java
@@ -6,11 +6,13 @@ import com.svj.dto.Order;
 import com.svj.dto.User;
 import com.svj.entity.Payment;
 import com.svj.repository.PaymentRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
+import java.util.function.Consumer;
 
 @Component
 public class OrderProcessingConsumer {
@@ -26,34 +28,37 @@ public class OrderProcessingConsumer {
         this.repository= repository;
     }
 
-    @KafkaListener(topics = "ORDER_PAYMENT_TOPIC")
-    public void processOrder(String orderJsonString){
-        try {
-            Order order = objectMapper.readValue(orderJsonString, Order.class);
-            // build payment request without payment status. For Cash on Delivery payment will be success only after cash collection.
-            Payment payment = Payment.builder()
-                    .payMode(order.getPaymentMode())
-                    .amount(order.getPrice())
-                    .paidDate(LocalDate.now())
-                    .userId(order.getUserId())
-                    .orderId(order.getOrderId())
-                    .build();
-            if(payment.getPayMode().equals("COD")){
-                payment.setPaymentStatus("PENDING");
-            }else {
-                // validate available amount
-                User user = restTemplate.getForObject(USER_SREVICE_URL + payment.getUserId(), User.class);
-                if(payment.getAmount()> user.getAvailableAmount()){
-                    throw new RuntimeException("Insufficient amount!");
-                }else{
-                    payment.setPaymentStatus("PAID");
-                    restTemplate.put(USER_SREVICE_URL+payment.getUserId()+"/"+payment.getAmount(), null);
-                }
+//    @KafkaListener(topics = "ORDER_PAYMENT_TOPIC")
+    @Bean
+    public Consumer<String> processOrder(){
+        return orderJsonString-> {
+            try {
+                Order order = objectMapper.readValue(orderJsonString, Order.class);
+                // build payment request without payment status. For Cash on Delivery payment will be success only after cash collection.
+                Payment payment = Payment.builder()
+                        .payMode(order.getPaymentMode())
+                        .amount(order.getPrice())
+                        .paidDate(LocalDate.now())
+                        .userId(order.getUserId())
+                        .orderId(order.getOrderId())
+                        .build();
+                if (payment.getPayMode().equals("COD")) {
+                    payment.setPaymentStatus("PENDING");
+                } else {
+                    // validate available amount
+                    User user = restTemplate.getForObject(USER_SREVICE_URL + payment.getUserId(), User.class);
+                    if (payment.getAmount() > user.getAvailableAmount()) {
+                        throw new RuntimeException("Insufficient amount!");
+                    } else {
+                        payment.setPaymentStatus("PAID");
+                        restTemplate.put(USER_SREVICE_URL + payment.getUserId() + "/" + payment.getAmount(), null);
+                    }
 
+                }
+                repository.save(payment);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace(); // log.error
             }
-            repository.save(payment);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace(); // log.error
-        }
+        };
     }
 }
